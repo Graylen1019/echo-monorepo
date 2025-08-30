8502fa22f103ea5917b924f8c9c62f15cc1f8406
Graylen <delilhabigelow@gmail.com>
2025-08-30 03:28:34 -0400

    Clerk Setup and initilization of protected routes.

---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return ( 
+        <div className="h-screen w-full flex justify-center items-center max-w-md mx-auto">
+            <p className="text-2xl text-semibold text-[#2a2a2a]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur exercitationem fuga architecto quibusdam!</p>
+        </div>
+     );
+}
+ 
+export default Page;
